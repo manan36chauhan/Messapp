200bdc23ffc8d1204d05708f05aa6c58711318d5
manan36chauhan <cmanan36@gmail.com>
2024-10-03 18:11:46 +0530

    First commit

---
+++ Messapp/Controllers/ProfileViewerViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ProfileViewerViewController.swift
+//  Messapp
+//
+//  Created by MacBookPro on 03/10/24.
+//
+
+import UIKit
+
+class ProfileViewerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
